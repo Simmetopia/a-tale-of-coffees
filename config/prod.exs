af2b6dc0be0d4592c0eb6bd0c99af3db4b9fcd2e
simmetopia <seg@it-minds.dk>
2021-04-19 08:50:43 +0200

    no forcing of ssl

--- config/prod.exs
+++ config/prod.exs
@@ -45,7 +45,7 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-config :a_tale_of_coffees, ATaleOfCoffeesWeb.Endpoint, force_ssl: [hsts: true]
+# config :a_tale_of_coffees, ATaleOfCoffeesWeb.Endpoint, force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
